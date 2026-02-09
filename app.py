e64829fc333601deb85a7334d8b47433d4566fa9
Dhruv Thippasandra <74385082+Dhruvq@users.noreply.github.com>
2026-02-09 11:08:14 -0800

    AWS test

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "<h1>NetProphet is LIVE! 🚀</h1><p>System status: Online</p>"
+    return "<h1>NetProphet is LIVE!</h1><p>System status: Online</p>"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
