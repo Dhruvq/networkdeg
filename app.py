0138730b20422b801486090e6af134d20b4be668
Dhruv Thippasandra <74385082+Dhruvq@users.noreply.github.com>
2026-02-10 17:07:30 -0800

    minor fix in app.py

--- app.py
+++ app.py
@@ -74,4 +74,4 @@ def home():
         return render_template_string(HTML_TEMPLATE, error=str(e))
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001, debug=True)
+    app.run(host='0.0.0.0', port=5000)
